37a9cf9c22d0dcbe9822ab7689e91be47d44adc1
João Marques <jota@localhost.localdomain>
2019-11-10 00:05:43 +0000

    Workstation

--- workstation/install_everything.sh
+++ workstation/install_everything.sh
@@ -2,19 +2,17 @@
 
 # basic install
 echo "Installing base..."
-sudo dnf -y install vim git code flatpak make python-pip python3-pip neovim python{2,3}-neovim
+sudo dnf -y install vim git flatpak make python-pip python3-pip neovim python-neovim
 
 # docker
 echo "Installing docker..."
-sudo dnf install -y dnf-utils \
-  device-mapper-persistent-data \
-  lvm2
+sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf makecache
 
-sudo dnf-config-manager \
-    --add-repo \
-    https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf install docker-ce
 
-sudo dnf install docker-ce docker-ce-cli containerd.io
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
 
 # # spotify
 # echo "Installing spotify..."
@@ -28,7 +26,7 @@ sudo dnf install docker-ce docker-ce-cli containerd.io
 
 # Virtual Box
 echo "Installing vbox..."
-wget http://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo -P /etc/yum.repos.d/
+sudo wget http://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo -P /etc/yum.repos.d/
 
 sudo dnf update
 
@@ -36,7 +34,7 @@ sudo dnf install @development-tools
 sudo dnf install kernel-devel kernel-headers dkms qt5-qtx11extras  elfutils-libelf-devel zlib-devel
 sudo dnf install VirtualBox-6.0
 
-usermod -a -G vboxusers jota
+sudo usermod -a -G vboxusers jota
 
 # # Pop OS Theme
 # echo "Installing pop os theme..."
@@ -72,3 +70,15 @@ pip install --user pipenv
 # vim Plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Slack
+sudo dnf -y install wget
+wget https://downloads.slack-edge.com/linux_releases/slack-3.4.0-0.1.fc21.x86_64.rpm
+sudo dnf localinstall slack-3.4.0-0.1.fc21.x86_64.rpm
+rm slack-3.4.0-0.1.fc21.x86_64.rpm
+
+# VS Code
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo dnf check-update
+sudo dnf install code
