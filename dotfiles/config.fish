a45574467c3334031bfa63422c672713c8bca417
joao-p-marques <joao.marques.contacto@gmail.com>
2020-02-26 15:58:41 +0000

    add gitconfig current branch

--- dotfiles/config.fish
+++ dotfiles/config.fish
@@ -1,2 +1,5 @@
 set PATH /home/jota/.local/bin/ /snap/bin/ $PATH
 set -x EDITOR 'vim'
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/jota/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/home/jota/Downloads/google-cloud-sdk/path.fish.inc'; end
