c2f75ea2c12ea4d32b257d823c921020e4809366
joao-p-marques <joao.marques.contacto@gmail.com>
2020-03-09 13:16:18 +0000

    add alias for ll = ls -la

--- dotfiles/config.fish
+++ dotfiles/config.fish
@@ -3,3 +3,5 @@ set -x EDITOR 'vim'
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/home/jota/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/home/jota/Downloads/google-cloud-sdk/path.fish.inc'; end
+
+alias ll='ls -la'
