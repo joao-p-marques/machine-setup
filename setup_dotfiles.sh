8b2ab6b6a0aaf2cf1cbdf9959f59876ac7474504
João Marques <joao.marques@tecnativa.com>
2020-10-13 16:31:28 +0100

    update dotfiles

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -64,14 +64,6 @@ echo "Creating symlink to $file in ~/.config/Code/User/settings.json ."
 ln -s $dir/$file ~/.config/Code/User/settings.json
 echo "$file ...done"
 
-# Do the same for workspace settings
-file=workspaces-data.json
-echo "Moving any existing $file from ~/.var/app/com.github.devalien.workspaces/data/com.github.devalien.workspaces/ to $olddir"
-mv ~/.var/app/com.github.devalien.workspaces/data/com.github.devalien.workspaces/data.json ~/dotfiles_old/
-echo "Creating symlink to $file in ~/.var/app/com.github.devalien.workspaces/data/com.github.devalien.workspaces/data.json ."
-ln -s $dir/$file ~/.var/app/com.github.devalien.workspaces/data/com.github.devalien.workspaces/data.json
-echo "$file ...done"
-
 # Do the same for ulaucher settings
 echo "Moving any existing file from ~/.config/ulauncher to $olddir"
 mv ~/.config/ulauncher/*.json ~/dotfiles_old/
