6e9be0d84f57da63abcebc7f69d4397b58fa7484
João Marques <joao.marques@tecnativa.com>
2021-03-24 14:44:23 +0000

    update configuration files

--- dotfiles/config.fish
+++ dotfiles/config.fish
@@ -1,5 +1,4 @@
 set PATH /home/jota/.local/bin/ /snap/bin/ /var/lib/snapd/snap/bin /usr/lib/ $PATH
-set -x EDITOR 'vim'
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/home/jota/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/home/jota/Downloads/google-cloud-sdk/path.fish.inc'; end
@@ -9,12 +8,12 @@ alias rm='rm -I'
 alias v='vim'
 
 alias mvn='docker run -it --rm -v $PWD:/src -w /src maven mvn'
-alias npm='docker run -it --rm -v $PWD:/src -w /src node npm'
 alias yarn='docker run -it --rm -v $PWD:/src -w /src node yarn'
 
 alias vimconf='vim ~/.vimrc' 
 alias fishconf='vim ~/.config/fish/config.fish' 
 alias tmuxconf='vim ~/.tmux.conf' 
+alias codetest='code --user-data-dir ~/code_profiles/test/data/'
 
 bind \cH backward-kill-word
 bind "[3;5~" kill-word
